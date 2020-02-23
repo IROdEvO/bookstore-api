f7296800694f459b1a982c49ebb61a4e29ca925b
Asiri Iroshan <asiriiroshan@localhost.localdomain>
2020-02-23 11:24:25 +0530

    Updated and changed

--- src/modules/books/book.controller.ts
+++ src/modules/books/book.controller.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from 'express';
+import { Request, Response, response } from 'express';
 import Book from './book.schema';
 
 export const getBooks = async (req: Request, res: Response): Promise<void> => {
   const books = await Book.find({});
-  // TODO: your implementation here.
+  res.status(200).json(books);
 };
 
 export const getBookById = async (
@@ -11,11 +11,12 @@ export const getBookById = async (
   res: Response,
 ): Promise<void> => {
   const books = await Book.findById(req.params.bookId);
-  // TODO: your implementation here.
+  res.status(200).json(books);
 };
 
 export const addBook = async (req: Request, res: Response): Promise<void> => {
   let book = new Book(req.body);
   book = await book.save();
-  // TODO: your implementation here.
+  res.status(201).json(book);
+  
 };
